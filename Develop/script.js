fb8874624b1c5215250c0a830be52659fe56a3ba
SThevenot <sktmh007@gmail.com>
2022-08-07 19:44:01 -0500

    finished most of javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,87 @@
+/** @format */
+
 // Assignment code here
 
+// declared variables
+const lowerChars = "abcdefghijklmnopqrstuvwxyz";
+const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChars = "0123456789";
+const specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~\\";
+let charCount;
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//prompt function
+function promptValidate() {
+  charCount = prompt(
+    "How many characters would you like in your password? (8 to 128 characters)"
+  );
+  if (charCount < 8) {
+    alert("Please input a number between 8 and 128");
+    promptValidate();
+  }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  if (charCount > 128) {
+    alert("Please input a number between 8 and 128");
+    promptValidate();
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function confirmInput() {
+  let possibleCharacters = "";
+  //lowercase confirm
+  const lowerConfirm = confirm("do you want to include lowercase letters");
+  if (lowerConfirm) {
+    possibleCharacters += lowerChars;
+  }
+
+  //uppercase confirm
+
+  const upperConfirm = confirm("Do you want to include uppercase letters");
+  if (upperConfirm) {
+    possibleCharacters += upperChars;
+  }
+
+  //numbers confirm
+  const numberConfirm = confirm("do you want to include numbers");
+  if (numberConfirm) {
+    possibleCharacters += numberChars;
+  }
+
+  //special characters confirm
+  const specialConfirm = confirm("do you want to include special characters");
+  if (specialConfirm) {
+    possibleCharacters += specialChars;
+  }
+
+  if (
+    lowerConfirm == false &&
+    upperConfirm == false &&
+    numberConfirm == false &&
+    specialConfirm == false
+  ) {
+    alert("Please select one of the following");
+    confirmInput();
+  }
+
+  return possibleCharacters;
+}
+
+//function is used to take the number types in the prompt and create a random string of numbers/letters/characters based on the users selection
+
+function createPassword(charactersChosen) {
+  let returnString = "";
+  for (var i = 1; i <= charCount; i++) {
+    returnString +=
+      charactersChosen[
+        Math.floor(Math.random() * (charactersChosen.length + 1))
+      ];
+  }
+  alert(returnString);
+}
+
+// Write password to the #password input, this is where all of the functions above are used and triggered when the button is clicked
+function writePassword() {
+  promptValidate();
+  let userSelectedCharacters = confirmInput();
+  let charactersChosen = userSelectedCharacters.split("");
+  createPassword(charactersChosen);
+}
--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 This module challenge is focused on creating a series of functions in javascript in order to generate a password based on the user's specifications. 
 
 When the 'generate password' button is clicked, the user will be prompted to type in how many characters they want the password to be. After a number betweeen 8 and 128 is types, they will face a series of 'confirm' boxes and most select at least one of the questions to include lowercase, uppercase, numbers, or special characters in their password. Once they've completed this, they will receive an alert with their randomly generated password.
+
+
+Repo Link : https://github.com/SThevenot/random-password
+Deployed Link : https://sthevenot.github.io/random-password/
+
+![Screenshot 1](./images/screenshot-1.png)
+![Screenshot 2](./images/screenshot-2.png)
